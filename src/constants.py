2df0387d8cae8fc3ff75207832dfaefdbdeeab52
Alexander Hurst <alexander@ahmail.ca>
2019-02-12 23:43:43 -0500

    minor additions, corrections

--- src/constants.py
+++ src/constants.py
@@ -1,20 +1,20 @@
 import math
 
 # bounds of the window, in lat/long
-LEFTLON = -78.9162
-RIGHTLON = -78.8250
-TOPLAT = 43.9711
-BOTLAT = 43.8853
+LEFTLON = -78.9250
+RIGHTLON = -78.7900
+TOPLAT = 43.9500
+BOTLAT = 43.8450
 WIDTH = RIGHTLON-LEFTLON
 HEIGHT = TOPLAT-BOTLAT
+HGT_BOT = math.floor(BOTLAT)
+HGT_LEFT = math.floor(LEFTLON)
 # ratio of one degree of longitude to one degree of latitude
 LONRATIO = math.cos(TOPLAT*3.1415/180)
 WINWID = 800
 WINHGT = (int)((WINWID/LONRATIO)*HEIGHT/WIDTH)
 TOXPIX = WINWID/WIDTH
 TOYPIX = WINHGT/HEIGHT
-# width,height of elevation array
-EPIX = 3601
 # approximate number of meters per degree of latitude
 MPERLAT = 111000
 MPERLON = MPERLAT*LONRATIO
